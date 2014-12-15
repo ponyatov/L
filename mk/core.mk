4abd3a91641352efe028aaa413f0b734619f72fe
Dmitry Ponyatov <dponyatov@gmail.com>
2014-12-15 15:39:34 +0400

    + kernel/config + kernel/app/micro - kernel/qemu/realtime
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/core.mk
+++ mk/core.mk
@@ -3,29 +3,31 @@ CFG_KERNEL = \
 	INSTALL_HDR_PATH=$(ROOT)
 	
 .PHONY: core
-core: kernel libc bb
+core: kernel ulibc bb
 
 .PHONY: kernel
 kernel: $(SRC)/$(KERNEL)/README
-
+	# 1
 	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) distclean
 	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) allnoconfig
-
+	# 2
 	cat kernel/all >> $(SRC)/$(KERNEL)/.config
 	cat kernel/arch/$(ARCH) >> $(SRC)/$(KERNEL)/.config
 	cat kernel/cpu/$(CPU) >> $(SRC)/$(KERNEL)/.config
 	cat kernel/hw/$(HW) >> $(SRC)/$(KERNEL)/.config
 	cat kernel/app/$(APP) >> $(SRC)/$(KERNEL)/.config
-	
+	# 3
 	echo "CONFIG_CROSS_COMPILE=\"$(CPU)-\"" >> $(SRC)/$(KERNEL)/.config
 	echo "CONFIG_LOCALVERSION=\"-$(HW)$(APP)\"" >> $(SRC)/$(KERNEL)/.config
 	echo "CONFIG_DEFAULT_HOSTNAME=\"$(HW)$(APP)\"" >> $(SRC)/$(KERNEL)/.config	
-
+	# 4
 	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) menuconfig
+	# 5
 	cd $(SRC)/$(KERNEL) && $(XPATH) $(MAKE) $(CFG_KERNEL)
-	
+	# 6
 	make kernel-$(ARCH)-fix
 	cp $(SRC)/$(KERNEL)/arch/x86/boot/zImage $(BOOT)/$(HW)$(APP).kernel
+	# 7
 	
 .PHONY: kernel-i386-fix
 kernel-i386-fix:
