d96c8ec7f3eb0fd1d616cb9b5e63b55985a66a6e
Dmitry Ponyatov <dponyatov@gmail.com>
2014-12-15 17:23:17 +0400

    +ulibc
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/core.mk
+++ mk/core.mk
@@ -1,7 +1,8 @@
-CFG_KERNEL = \
-	ARCH=$(ARCH) \
-	INSTALL_HDR_PATH=$(ROOT)
+CFG_KERNEL = ARCH=$(ARCH) INSTALL_HDR_PATH=$(ROOT)
 	
+CFG_ULIBC = CROSS=$(CPU)- ARCH=$(ARCH) PREFIX=$(ROOT)
+#	UCLIBC_EXTRA_CFLAGS=""	
+
 .PHONY: core
 core: kernel ulibc bb
 
@@ -28,6 +29,7 @@ kernel: $(SRC)/$(KERNEL)/README
 	make kernel-$(ARCH)-fix
 	cp $(SRC)/$(KERNEL)/arch/x86/boot/zImage $(BOOT)/$(HW)$(APP).kernel
 	# 7
+	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) headers_install
 	
 .PHONY: kernel-i386-fix
 kernel-i386-fix:
@@ -36,6 +38,30 @@ kernel-i386-fix:
 	
 .PHONY: ulibc
 ulibc: $(SRC)/$(ULIBC)/README
+	# 1
+	cd $(SRC)/$(ULIBC) && $(XPATH) make $(CFG_ULIBC) distclean
+	cd $(SRC)/$(ULIBC) && $(XPATH) make $(CFG_ULIBC) allnoconfig
+	# 2
+	cat ulibc/all >> $(SRC)/$(ULIBC)/.config
+	cat ulibc/arch/$(ARCH) >> $(SRC)/$(ULIBC)/.config
+	cat ulibc/cpu/$(CPU) >> $(SRC)/$(ULIBC)/.config
+	cat ulibc/hw/$(HW) >> $(SRC)/$(ULIBC)/.config
+	cat ulibc/app/$(APP) >> $(SRC)/$(ULIBC)/.config
+	# 3
+	echo "KERNEL_HEADERS=\"$(ROOT)/include\"" >> $(SRC)/$(ULIBC)/.config
+	echo "CROSS_COMPILER_PREFIX=\"$(CPU)-\"" >> $(SRC)/$(ULIBC)/.config
+	# 4
+	cd $(SRC)/$(ULIBC) && make $(CFG_ULIBC) menuconfig
+	# 5
+	cd $(SRC)/$(ULIBC) && $(XPATH) $(MAKE) $(CFG_ULIBC)
+	# 6
+	cd $(SRC)/$(ULIBC) && $(XPATH) $(MAKE) $(CFG_ULIBC) install
+
+#	$(MAKE) $(CFG_LIBC) install_utils && \
+#	$(MAKE) $(CFG_LIBC) hostutils
+#	cp $(SRC)/$(LIBC)/utils/ldd.host      $(TC)/bin/$(HW)-ldd
+#	cp $(SRC)/$(LIBC)/utils/ldconfig.host $(TC)/bin/$(HW)-ldconfig
+#	cp $(SRC)/$(LIBC)/utils/getconf.host  $(TC)/bin/$(HW)-getconf
 
 .PHONY: bb
 bb: $(SRC)/$(BB)/README
