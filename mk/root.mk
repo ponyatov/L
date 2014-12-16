4f6c2421bb721515d9696058f943e81a504180f3
Dmitry Ponyatov <dponyatov@gmail.com>
2014-12-16 16:19:10 +0400

    + root share etc
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/root.mk
+++ mk/root.mk
@@ -1,3 +1,17 @@
+ROOTREX = "./(boot|include|lib/.*\.[ao])"
+
 .PHONY: root
 root:
+	# 1
+	rm -rf $(ETC) ; cp -r etc $(ROOT)/
+	cp README.md $(ETC)/
+	chmod +x $(ETC)/init.d/*
+	# 2	
 	$(XPATH) $(LDCONFIG) -v -r $(ROOT)
+	# 3
+#	ln -fs /sbin/init $(ROOT)/init
+	# 4
+	cp -r share $(ROOT)/
+	# 5
+	cd $(ROOT) && find . | egrep -v $(ROOTREX) | cpio -o -H newc > $(BOOT)/$(HW)$(APP).cpio
+	cat $(BOOT)/$(HW)$(APP).cpio | gzip -9 > $(BOOT)/$(HW)$(APP).rootfs
