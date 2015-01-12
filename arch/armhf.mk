89dbcc57cdae2d73f12479810cca529e7781f628
Dmitry Ponyatov <dponyatov@gmail.com>
2015-01-12 14:46:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/armhf.mk
+++ arch/armhf.mk
@@ -1,2 +1,4 @@
-TARGET = arm-linux-uclibceabihf
-CFG_ARCH = --enable-interwork --with-float=hard
+TARGET = arm-linux-uclibceabi
+#arm-linux-uclibceabihf
+CFG_ARCH = --enable-interwork 
+#--with-float=hard
