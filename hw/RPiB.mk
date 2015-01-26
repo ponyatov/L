286e561c4a7dca88823ae43c47d9f3936b6bb6de
Dmitry Ponyatov <dponyatov@gmail.com>
2015-01-26 12:58:02 +0400

    err: bcm3835 not boots under qemu
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/RPiB.mk
+++ hw/RPiB.mk
@@ -1,5 +1,9 @@
 # Raspberry Pi model B
 CPU = BCM2835
-QEMU_CFG = -M versatilepb -cpu arm1176 -m 32M -net none \
-	-serial stdio \
+QEMU_CFG = -M versatilepb -cpu arm1176 -m 64M -net none \
+	-serial file:serial.log \
 	-append "console=ttyAMA0,115200"
+#	console=ttyAMA0,115200"
+#console=ttyAMA0,115200 kgdboc=ttyAMA0	
+#earlycon=smh
+# -nographic
