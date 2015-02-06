06b8a18af0aaa31ff4e263e4208dfc6c9254a2f7
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-06 13:09:09 +0400

    + pack/strace2files * canadian:   * --prefix=$(USR)   * T P O   + bin486, bin486-pack
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/sdk/canadian.mk
+++ mk/sdk/canadian.mk
@@ -1,35 +1,36 @@
 # canadian cross
 
-CFG_CANADIAN = 
+CFG_CANADIAN = --prefix=$(USR)
 #--libdir=$(ROOT)/lib --bindir=$(ROOT)/bin
 
 .PHONY: canadian
 canadian: $(SRC)/$(BINUTILS)/README
 	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
 	cd $(TMP)/$(BINUTILS) &&\
-	$(XPATH) $(SRC)/$(BINUTILS)/$(TCFG) $(CFG_CANADIAN) $(P) $(T) &&\
-	$(MAKE) && $(XPATH) make install-strip
+	$(XPATH) $(SRC)/$(BINUTILS)/$(TCFG) \
+		$(CFG_CANADIAN) $(O) \
+		--target=$(T) --program-prefix=$(P) &&\
+	$(MAKE) &&\
+	$(XPATH) strace -f -e trace=file -o $(PWD)/pack/$(S).strace make install-strip
 
 .PHONY: binhost
 binhost:
-	make canadian \
-		P="--prefix=$(USR)" \
-		T="--target=$(TARGET) --program-prefix=$(TARGET)- $(CFG_ARCH) $(CFG_CPU)"
+	make canadian T=$(TARGET) P= O="$(CFG_ARCH) $(CFG_CPU)"
+
+.PHONY: bin486
+bin486:
+	make canadian S=$@ T=i486-elf P=i486- O="--with-cpu=i486dx"
+
+.PHONY: bin486-pack
+bin486-pack:
+	./pack/strace2files.py $(ROOT)/ < pack/bin486.strace > pack/bin486.files
+	cd $(ROOT) && tar cmpl -T ../pack/bin486.files > ../pack/$(HW)$(APP)_bin486.tgz
 
 .PHONY: binavr
 binavr:
-	make canadian \
-		P="--prefix=$(USR)" \
-		T="--target=avr --program-prefix=avr-"
-#		 $(CFG_ARCH) $(CFG_CPU)"
+	make canadian T=avr P=avr- O=
 
-#BINHOST_CFG = --target=$(TARGET) $(CFG_ARCH) $(CFG_CPU) \
-#	--prefix=$(USR) --disable-werror
-#
-#.PHONY: binhost
-#binhost: $(SRC)/$(BINUTILS)/README
-#	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
-#	cd $(TMP)/$(BINUTILS) &&\
-#	$(XPATH) $(SRC)/$(BINUTILS)/$(XCFG) $(BINHOST_CFG) &&\
-#	$(XPATH) $(MAKE) &&\
-#	$(XPATH) make install-strip
+.PHONY: bincm3
+bincm3:
+	make canadian T=arm-none-eabi P=cm3 \
+		O="--with-cpu=cortex-m3 --enable-interwork"
