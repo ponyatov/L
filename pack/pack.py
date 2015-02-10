504b6422660d5003c1989a1b3a79b74963ce4c17
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-10 17:57:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pack/pack.py
+++ pack/pack.py
@@ -6,8 +6,22 @@ PACK=sys.argv[1]
 ALLFILES=open('%s/allfiles'%PACK)
 ROOTFILES=open('%s/rootfiles'%PACK,'w')
 
+R={}
+P={}
+
+for i in filter(lambda x:x.split('.')[-1]=='rex',os.listdir('pack')):
+    PN=i.split('.')[0]
+    P[PN]=open('%s/%s.files'%(PACK,PN),'w')
+    R[PN]=map(lambda x:x[:-1],open('pack/%s.rex'%PN).readlines())
+
 while 1:
     F=ALLFILES.readline()[:-1]
     if not F: break
-    print >>ROOTFILES,F
-
+    T=0
+    for i in R:
+        for j in R[i]:
+            if re.match(j,F):
+                T=1
+                print >>P[i],F
+    if not T:
+        print >>ROOTFILES,F
