040415dc7a2eac052e6f7324c7ee4546110ef65c
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-27 23:07:10 +0400

    +src/%.tgz
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/src.mk
+++ mk/src.mk
@@ -5,3 +5,5 @@ $(SRC)/%/README: $(GZ)/%.tar.bz2
 	cd $(SRC) && bzcat $< | tar x && touch $@
 $(SRC)/%/README: $(GZ)/%.tar.xz
 	cd $(SRC) && xzcat $< | tar x && touch $@
+$(SRC)/%/README: $(GZ)/%.tgz
+	cd $(SRC) &&  zcat $< | tar x && touch $@
