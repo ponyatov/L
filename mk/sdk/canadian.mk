f18a5c78b4dbd4abd649b044a44b28d09d04cf9b
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-27 22:28:26 +0400

    gcc error: cstring no such file or directory
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/sdk/canadian.mk
+++ mk/sdk/canadian.mk
@@ -10,19 +10,19 @@ CFG_CAN_GCC = $(CFG_CAN_BIN) \
 	
 .PHONY: canadian
 canadian: $(SRC)/$(BINUTILS)/README $(SRC)/$(GCC)/README
-	# binutils
-	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
-	cd $(TMP)/$(BINUTILS) &&\
-	$(XPATH) $(SRC)/$(BINUTILS)/$(CACFG) \
-		$(CFG_CAN_BIN) --target=$(T) $(O) --prefix=$(PFX) --with-sysroot=$(SR) &&\
-	$(MAKE) && $(PINSTALL)-strip &&\
-	grep $(ROOT) $(PACK)/.strace > $(PACK)/binutils.$(PK).strace && rm $(PACK)/.strace
-#	# gcc
-#	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
-#	cd $(TMP)/$(GCC) &&\
-#	$(XPATH) $(SRC)/$(GCC)/$(CACFG) \
-#		$(CFG_CAN_GCC) --target=$(T) $(O) --prefix=$(PFX) --with-sysroot=$(SR)
-#	cd $(TMP)/$(GCC) && $(XPATH) $(MAKE) all-gcc
+#	# binutils
+#	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
+#	cd $(TMP)/$(BINUTILS) &&\
+#	$(XPATH) $(SRC)/$(BINUTILS)/$(CACFG) \
+#		$(CFG_CAN_BIN) --target=$(T) $(O) --prefix=$(PFX) --with-sysroot=$(SR) &&\
+#	$(MAKE) && $(PINSTALL)-strip &&\
+#	grep $(ROOT) $(PACK)/.strace > $(PACK)/binutils.$(PK).strace && rm $(PACK)/.strace
+	# gcc
+	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
+	cd $(TMP)/$(GCC) &&\
+	$(XPATH) $(SRC)/$(GCC)/$(CACFG) \
+		$(CFG_CAN_GCC) --target=$(T) $(O) --prefix=$(PFX) --with-sysroot=$(SR)
+	cd $(TMP)/$(GCC) && $(XPATH) $(MAKE) all-gcc
 #	cd $(TMP)/$(GCC) && $(XPATH) $(MAKE) install-gcc
 #	cd $(TMP)/$(GCC) && $(XPATH) $(MAKE) all-target-libgcc
 #	cd $(TMP)/$(GCC) && $(XPATH) $(MAKE) install-target-libgcc
