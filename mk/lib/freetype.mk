a01ef0429ace3538bcf9c6da8d7b0f7005f50f3b
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-01 14:16:04 +0400

    * freetype
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/lib/freetype.mk
+++ mk/lib/freetype.mk
@@ -5,4 +5,5 @@ freetype: $(SRC)/$(FREETYPE)/README
 	rm -rf $(TMP)/$(FREETYPE) && mkdir $(TMP)/$(FREETYPE) &&\
 	cd $(TMP)/$(FREETYPE) &&\
 	$(XPATH) $(SRC)/$(FREETYPE)/$(TCFG) $(CFG_FREETYPE) &&\
-	$(MAKE) && $(INSTALL)
+	$(MAKE) && $(INSTALL) &&\
+	mv -f $(ROOT)/bin/$@-config $(TC)/bin/
--- mk/lib/sdl.mk
+++ mk/lib/sdl.mk
@@ -1,7 +1,6 @@
 CFG_SDL_MAIN = $(CFG_CPU) \
 	--disable-audio \
-	--disable-joystick \
-	--disable-audio
+	--disable-joystick
  
 #	--disable-assembly \
 #	--enable-video-fbcon
@@ -19,7 +18,7 @@ sdl_main: $(SRC)/$(SDL)/README
 	rm -rf $(TMP)/$(SDL) && mkdir $(TMP)/$(SDL) &&\
 	cd $(TMP)/$(SDL) &&\
 	$(XPATH) $(SRC)/$(SDL)/$(TCFG) $(CFG_SDL_MAIN) &&\
-	$(XPATH) $(MAKE) install &&\
+	$(MAKE) && $(INSTALL) &&\
 	mv -f $(ROOT)/bin/sdl-config $(TC)/bin/
 #	mv -f $(ROOT)/share/aclocal $(TC)/share/
 #	mv -f $(ROOT)/share/man/man3 $(TC)/share/man/
@@ -29,15 +28,14 @@ sdl_image: $(SRC)/$(SDL_IMAGE)/README
 	rm -rf $(TMP)/$(SDL_IMAGE) && mkdir $(TMP)/$(SDL_IMAGE) &&\
 	cd $(TMP)/$(SDL_IMAGE) &&\
 	$(XPATH) $(SRC)/$(SDL_IMAGE)/$(TCFG) $(CFG_SDL_IMAGE) &&\
-	$(XPATH) $(MAKE) install
+	$(MAKE) && $(INSTALL)
 	
 .PHONY: sdl_ttf
 sdl_ttf: $(SRC)/$(SDL_TTF)/README
 	rm -rf $(TMP)/$(SDL_TTF) && mkdir $(TMP)/$(SDL_TTF) &&\
 	cd $(TMP)/$(SDL_TTF) &&\
-	$(XPATH) $(SRC)/$(SDL_TTF)/$(TCFG) $(CFG_SDL_TTF) 
-#	&&\
-#	$(XPATH) $(MAKE) install
+	$(XPATH) $(SRC)/$(SDL_TTF)/$(TCFG) $(CFG_SDL_TTF)  
+#	$(MAKE) && $(INSTALL) &&\
 	
 ##$(SRC)/$(SDL)/README-SDL.txt: 
 ##etc/README-SDL.txt:  
