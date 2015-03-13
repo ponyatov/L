a78c4c989f8de62844c1b5ff74cac384582073fa
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-13 17:17:06 +0400

    + qemu net user + kernel intel e1000 eth (qemulated) + bb: udhcpc
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/x86.mk
+++ hw/x86.mk
@@ -1,6 +1,6 @@
 # universal i686 PC (32 bit) PII+
 CPU = i686
-QEMU_CFG = -m 64M -net none -localtime
+QEMU_CFG = -m 64M -net nic -net user -localtime
 #vga=ask
 #0x312 640x480x24	
 #0x315 800x600x24	
