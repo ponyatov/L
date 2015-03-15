28cf4f42d484f1be983b406631d071e63689d7ca
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-16 00:29:48 +0400

    + kernel/all/ipc config section + enabled kernel/ sys V ipc & pty unix98 ( ... tring to build f**ed python
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/sdk/python.mk
+++ mk/sdk/python.mk
@@ -1,13 +1,27 @@
 
-CFG_PYTHON =
+CFG_PYTHON = --disable-ipv6 ac_cv_file__dev_ptmx=no \
+	ac_cv_file__dev_ptc=no
 .PHONY: python
 python: $(SRC)/$(PYTHON)/README
+#	rm -rf $(TMP)/$(PYTHON) && mkdir $(TMP)/$(PYTHON)
 #	# apply xcompile patch
 #	cd $(SRC)/$(PYTHON) && patch -N -p1 < $(GZ)/Python-$(PYTHON_VER)-xcompile.patch
-	# hot-part build
-	rm -rf $(TMP)/$(PYTHON) && mkdir $(TMP)/$(PYTHON) &&\
-	cd $(TMP)/$(PYTHON) &&\
-	$(SRC)/$(PYTHON)/configure && make python Parser/pgen
+#	# host-part build
+#	cd $(TMP)/$(PYTHON) &&\
+#	$(SRC)/$(PYTHON)/configure && make python Parser/pgen
+#	cd $(TMP)/$(PYTHON) && mv python hostpython && mv Parser/pgen Parser/hostpgen
+	-cd $(TMP)/$(PYTHON) && make distclean
+	cd $(TMP)/$(PYTHON) && $(XPATH) \
+	CC="$(TCC)" CXX="$(TCXX)" \
+	AR=$(TARGET)-ar RANLIB=$(TARGET)-ranlib \
+	$(SRC)/$(PYTHON)/configure --prefix=$(ROOT)/python \
+	--host=$(TARGET) --build=$(shell gcc -dumpmachine) \
+	$(CFG_PYTHON) 
+	cd $(TMP)/$(PYTHON) && $(XPATH) \
+	make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen \
+	BLDSHARED="$(TCC) -shared" CROSS_COMPILE=$(TARGET)- \
+	CROSS_COMPILE_TARGET=yes HOSTARCH=$(TARGET) \
+	BUILDARCH=$(shell gcc -dumpmachine)
 
 CFG_PYMITE = CC="$(TCC)" 
 .PHONY: pymite
