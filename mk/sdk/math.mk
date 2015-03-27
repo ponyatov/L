da2213a1627ccba260aa7acb6a9e58ae798657d8
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-27 15:33:46 +0400

    +fortran
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/sdk/math.mk
+++ mk/sdk/math.mk
@@ -1,10 +1,22 @@
 
+CFG_FORTRAN = $(CFG_BINUTILS0) $(CFG_CCLIBS0) \
+	--enable-languages="fortran" --prefix=$(TC)/fortran
+#--enable-threads --enable-libgomp	
+.PHONY: fortran
+fortran: $(SRC)/$(GCC)/README
+	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
+	cd $(TMP)/$(GCC) &&\
+	$(SRC)/$(GCC)/$(BCFG) $(CFG_FORTRAN)
+	cd $(TMP)/$(GCC) && $(MAKE) all-gcc
+	cd $(TMP)/$(GCC) && $(MAKE) install-gcc
+#	cd $(TMP)/$(GCC) && $(MAKE)
+	
 CFG_ECL =
 
 .PHONY: ecl
 ecl: $(SRC)/$(ECL)/README
 	cd $(SRC)/$(ECL) && $(XPATH) ./$(BCFG) $(CFG_ECL) && $(MAKE)
-
+	
 CFG_OCTAVE = --prefix=$(ROOT)/octave --enable-lite-kernel --with-f2c
 # --disable-docs --help
 .PHONY: octave
