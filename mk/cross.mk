b5200e0b43cb8c70d7076bfb142970456bf933e3
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-27 15:09:54 +0400

    +gccf: gcc with fortran *octave: need cross fortran
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -23,6 +23,7 @@ CFG_GCC = $(CFG_BINUTILS0) $(CFG_CCLIBS0) \
 	--enable-libstdcxx-time \
 	--enable-libstdcxx-threads \
 	--enable-libstdcxx-pch
+CFG_GCCF = $(CFG_GCC) --enable-languages="c,c++,fortran"
 
 .PHONY: cross0
 cross0: binutils0 cclibs0 gcc0
@@ -73,6 +74,20 @@ gcc: $(SRC)/$(GCC)/README
 	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
 	cd $(TMP)/$(GCC) &&\
 	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC)
+	cd $(TMP)/$(GCC) && $(MAKE) all-gcc
+	cd $(TMP)/$(GCC) && $(MAKE) install-gcc
+	cd $(TMP)/$(GCC) && $(MAKE) all-target-libgcc
+	cd $(TMP)/$(GCC) && $(MAKE) install-target-libgcc
+	cd $(TMP)/$(GCC) && $(MAKE) all-target-libstdc++-v3
+	cd $(TMP)/$(GCC) && $(MAKE) install-target-libstdc++-v3
+	mkdir -p $(ROOT)/lib &&\
+	cp -a $(TC)/$(TARGET)/lib/libgcc_s* $(ROOT)/lib/
+
+.PHONY: gccf
+gccf: $(SRC)/$(GCC)/README
+	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
+	cd $(TMP)/$(GCC) &&\
+	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCCF)
 	cd $(TMP)/$(GCC) && $(MAKE) all-gcc
 	cd $(TMP)/$(GCC) && $(MAKE) install-gcc
 	cd $(TMP)/$(GCC) && $(MAKE) all-target-libgcc
--- mk/sdk/math.mk
+++ mk/sdk/math.mk
@@ -5,9 +5,13 @@ CFG_ECL =
 ecl: $(SRC)/$(ECL)/README
 	cd $(SRC)/$(ECL) && $(XPATH) ./$(BCFG) $(CFG_ECL) && $(MAKE)
 
-CFG_OCTAVE =
+CFG_OCTAVE = --prefix=$(ROOT)/octave --enable-lite-kernel --with-f2c
+# --disable-docs --help
 .PHONY: octave
 octave: $(SRC)/$(OCTAVE)/README
+	rm -rf $(TMP)/$(OCTAVE) && mkdir $(TMP)/$(OCTAVE) &&\
+	cd $(TMP)/$(OCTAVE) &&\
+	$(XPATH) $(SRC)/$(OCTAVE)/$(TCFG) $(CFG_OCTAVE)
 
 CFG_MAXIMA =
 .PHONY: maxima
