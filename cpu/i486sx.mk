e4ddaec2f07835df4777524eeb3818a0e77b64a6
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-29 09:55:42 +0400

    +CFLAGS_CPU +ulibc optimization
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpu/i486sx.mk
+++ cpu/i486sx.mk
@@ -6,3 +6,4 @@ TARGET = i486-linux-uclibc
 # target CPU configure params
 CFG_CPU = --with-cpu=i486 --with-tune=i486
 # --disable-mmx --disable-3dnow --disable-sse
+CFLAGS_CPU = -march=i486 -mtune=i486
