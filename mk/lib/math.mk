8acfebd9c718cad0043eb2ddd67ee0a936356542
Dmitry Ponyatov <dponyatov@gmail.com>
2015-03-30 18:12:15 +0400

    + lapack untested shared build
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/lib/math.mk
+++ mk/lib/math.mk
@@ -16,9 +16,19 @@ blas: $(SRC)/$(BLAS)/README
 	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -o $(LIB)/libblas.so *.f
 #	make clean && $(XPATH) $(MAKE) $(CFG_BLAS)
 
-CFG_LAPACK =
+CFG_LAPACK = \
+	FORTRAN="$(TFORTRAN)" LOADER="$(TFORTRAN)" \
+	OPTS="$(TOPT) -frecursive"
 .PHONY: lapack
-lapack: $(SRC)/$(LAPACK)/README
+lapack: $(SRC)/$(LAPACK)/make.inc
+	cd $(SRC)/$(LAPACK) &&\
+	$(XPATH) $(TFORTRAN) -shared -fPIC -g0 -O0 -o $(LIB)/liblapack.so SRC/*.f
+#	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -frecursive -o $(LIB)/liblapack.so SRC/*.f
+#	make clean &&\
+#	$(XPATH) $(MAKE) $(CFG_LAPACK) lapack_install
+$(SRC)/$(LAPACK)/make.inc: 
+	make $(SRC)/$(LAPACK)/README
+	cd $(SRC)/$(LAPACK) && cp make.inc.example make.inc
 
-.PHONY: atlas
-atlas: $(SRC)/ATLAS/README
+#.PHONY: atlas
+#atlas: $(SRC)/ATLAS/README
