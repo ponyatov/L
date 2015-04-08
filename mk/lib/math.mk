159d3b5499b4343edc321393de9ced193bda02c6
Dmitry Ponyatov <dponyatov@gmail.com>
2015-04-08 13:40:21 +0400

    * lapack all SRC/*.f to shared
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/lib/math.mk
+++ mk/lib/math.mk
@@ -16,19 +16,29 @@ blas: $(SRC)/$(BLAS)/README
 	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -o $(LIB)/libblas.so *.f
 #	make clean && $(XPATH) $(MAKE) $(CFG_BLAS)
 
-CFG_LAPACK = \
-	FORTRAN="$(TFORTRAN)" LOADER="$(TFORTRAN)" \
-	OPTS="$(TOPT) -frecursive"
+#CFG_LAPACK = \
+#	FORTRAN="$(TFORTRAN)" LOADER="$(TFORTRAN)" \
+#	OPTS="$(TOPT) -frecursive"
+
 .PHONY: lapack
-lapack: $(SRC)/$(LAPACK)/make.inc
-	cd $(SRC)/$(LAPACK) &&\
-	$(XPATH) $(TFORTRAN) -shared -fPIC -g0 -O0 -o $(LIB)/liblapack.so SRC/*.f
-#	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -frecursive -o $(LIB)/liblapack.so SRC/*.f
-#	make clean &&\
-#	$(XPATH) $(MAKE) $(CFG_LAPACK) lapack_install
-$(SRC)/$(LAPACK)/make.inc: 
-	make $(SRC)/$(LAPACK)/README
-	cd $(SRC)/$(LAPACK) && cp make.inc.example make.inc
+lapack: $(SRC)/$(LAPACK)/README
+	cd $(SRC)/$(LAPACK)/SRC &&\
+	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -frecursive -o $(LIB)/liblapack.so *.f
+
+#	for ff in *.f; do \
+#	$(XPATH) $(TFORTRAN) $(TOPT) -c $$ff; \
+#	done
+#	echo "$(LAPACK_FILES)"
+#	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -c *.f
+#lapack: $(SRC)/$(LAPACK)/make.inc
+#	cd $(SRC)/$(LAPACK) &&\
+#	$(XPATH) $(TFORTRAN) -shared -fPIC -g0 -O0 -o $(LIB)/liblapack.so SRC/*.f
+##	$(XPATH) $(TFORTRAN) -shared -fPIC $(TOPT) -frecursive -o $(LIB)/liblapack.so SRC/*.f
+##	make clean &&\
+##	$(XPATH) $(MAKE) $(CFG_LAPACK) lapack_install
+#$(SRC)/$(LAPACK)/make.inc: 
+#	make $(SRC)/$(LAPACK)/README
+#	cd $(SRC)/$(LAPACK) && cp make.inc.example make.inc
 
 #.PHONY: atlas
 #atlas: $(SRC)/ATLAS/README
