1cbf42e22504c2a91081f11b8c70c48836249ccb
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-04 18:28:19 +0400

    * octave build with gettimeofday clobbering disabled
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/octave.mk
+++ mk/math/octave.mk
@@ -1,6 +1,7 @@
 CFG_OCTAVE = --prefix=$(ROOT)/octave --disable-docs \
 	--disable-readline --disable-java \
-	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)"
+	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)" \
+	gl_cv_func_gettimeofday_clobber=no
 .PHONY: octave
 octave: $(SRC)/$(OCTAVE)/README
 	rm -rf $(TMP)/$(OCTAVE) && mkdir $(TMP)/$(OCTAVE) &&\
