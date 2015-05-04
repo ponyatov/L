70b0a3ece6cde27c3329c32f3658a821cbf41725
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-04 18:37:02 +0400

    + octave extra ac flags for later tests (commented)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/octave.mk
+++ mk/math/octave.mk
@@ -2,6 +2,8 @@ CFG_OCTAVE = --prefix=$(ROOT)/octave --disable-docs \
 	--disable-readline --disable-java \
 	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)" \
 	gl_cv_func_gettimeofday_clobber=no
+# ?octave_cv_fortran_integer_size
+# ?octave_cv_ieee754_data_format
 .PHONY: octave
 octave: $(SRC)/$(OCTAVE)/README
 	rm -rf $(TMP)/$(OCTAVE) && mkdir $(TMP)/$(OCTAVE) &&\
