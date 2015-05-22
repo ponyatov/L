8e0fe48504fbf08390dc7491b844c0eb78b5a310
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-22 15:32:47 +0400

    * octave: fixed blas/lapack problem, configure works ok
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/octave.mk
+++ mk/math/octave.mk
@@ -1,9 +1,8 @@
 CFG_OCTAVE = --prefix=$(ROOT)/octave \
-	--with-blas="-lblas" --with-lapack="-llapack" 
-# --disable-docs \
-#	--disable-readline --disable-java \
-#	  \
-#	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)" \
+	--with-blas="-lblas" --with-lapack="-llapack" \
+	--disable-docs --disable-readline \
+	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)"
+#	 --disable-java \
 #	gl_cv_func_gettimeofday_clobber=no
 ## ?octave_cv_fortran_integer_size
 ## ?octave_cv_ieee754_data_format
