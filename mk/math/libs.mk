05a8d4a702b6a5a7fc11523bb4a297e51873464a
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-22 15:17:34 +0400

    *v blas/lapack static-only
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/libs.mk
+++ mk/math/libs.mk
@@ -1,50 +1,33 @@
 
 CFG_LABLA = \
 	FORTRAN="$(TFORTRAN)" OPTS="$(TOPT)" \
-	ARCH=$(TARGET)-ar RANLIB=$(TARGET)-ranlib
-# LOADER="$(TFORTRAN)"
+	LOADER="$(TFORTRAN)" \
+	ARCH=$(TARGET)-ar ARCHFLAGS=cr \
+	RANLIB=$(TARGET)-ranlib
 
 CFG_BLAS = $(CFG_LABLA) BLASLIB=$(LIB)/libblas.a
 .PHONY: blas
 blas: $(SRC)/$(BLAS)/README
 	cd $(SRC)/$(BLAS) && touch make.inc &&\
-	$(MAKE) clean &&\
-	$(MAKE) $(CFG_BLAS) &&\
-	echo $(XPATH) $(TFORTRAN) $(TOPT) -shared -fPIC -o $(LIB)/libblas.so *.f
+	$(MAKE) clean && $(MAKE) $(CFG_BLAS)
+#	$(XPATH) $(TFORTRAN) $(TOPT) -shared -fPIC -o $(LIB)/libblas.so *.f
 #		-z muldefs -Wl,-soname,libblas.so
 
-CFG_LAPACK = $(CFG_LABLA) OPTS="$(TOPT) -frecursive" \
-	LAPACKLIB=liblapack.a TMGLIB=libtmg.a
+CFG_LAPACK = $(CFG_LABLA) LAPACKLIB=$(LIB)/liblapack.a \
+	OPTS="$(TOPT) -frecursive" NOOPT="-g0 -O0 -frecursive" \
+	TIMER=INT_ETIME
+# TMGLIB=libtmg.a
 .PHONY: lapack
 lapack: $(SRC)/$(LAPACK)/README
 	cd $(SRC)/$(LAPACK) && touch make.inc &&\
-	$(MAKE) $(CFG_LAPACK) clean &&\
-	$(MAKE) $(CFG_LAPACK) lib
-
-#	cd $(SRC)/$(LAPACK)/SRC &&\
-#	$(XPATH) $(TFORTRAN) $(TOPT) -shared -fPIC \
-#		-o $(LIB)/liblapack.so \
+	$(MAKE) clean && $(MAKE) $(CFG_LAPACK) azlin
+#	$(XPATH) $(TFORTRAN) $(TOPT) -shared -fPIC -o $(LIB)/liblapack.so \
 #		*.f ../INSTALL/?lamch.f ../INSTALL/lsame.f
 
-#	OPTS="$(TOPT) -frecursive" NOOPT="$(TOPT) -frecursive"
-CFG_LAPLAP = \
-	FORTRAN="$(TFORTRAN)" LOADER="$(TFORTRAN)" \
-	ARCH="$(TARGET)-ar" RANLIB="$(TARGET)-ranlib" \
-	OPTS="-O0 -g0" NOOPT="-O0 -g0"
-.PHONY: laplap
-laplap: $(SRC)/$(LAPACK)/README $(SRC)/$(LAPACK)/make.inc
-	cd $(SRC)/$(LAPACK) && make clean
-	cd $(SRC)/$(LAPACK) && $(XPATH) $(MAKE) $(CFG_LAPLAP) blaslib
-	cd $(SRC)/$(LAPACK) && $(XPATH) $(MAKE) $(CFG_LAPLAP) lapacklib
-	mv $(SRC)/$(LAPACK)/librefblas.a $(LIB)/
-	mv $(SRC)/$(LAPACK)/liblapack.a $(LIB)/
-$(SRC)/$(LAPACK)/make.inc: $(SRC)/$(LAPACK)/INSTALL/make.inc.gfortran
-	cp -f $< $@ 
-
-CFG_GSL = CFLAGS="$(TOPT)"
-.PHONY: gsl
-gsl: $(SRC)/$(GSL)/README
-	rm -rf $(TMP)/$(GSL) && mkdir $(TMP)/$(GSL) &&\
-	cd $(TMP)/$(GSL) &&\
-	$(XPATH) $(SRC)/$(GSL)/$(TCFG) $(CFG_GSL) &&\
-	$(MAKE) && $(INSTALL)-strip && $(MVCONFIG)
+#CFG_GSL = CFLAGS="$(TOPT)"
+#.PHONY: gsl
+#gsl: $(SRC)/$(GSL)/README
+#	rm -rf $(TMP)/$(GSL) && mkdir $(TMP)/$(GSL) &&\
+#	cd $(TMP)/$(GSL) &&\
+#	$(XPATH) $(SRC)/$(GSL)/$(TCFG) $(CFG_GSL) &&\
+#	$(MAKE) && $(INSTALL)-strip && $(MVCONFIG)
