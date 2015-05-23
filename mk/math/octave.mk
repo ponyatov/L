b811a63cae4cad22a4af2becbf96a10c7fd80762
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-24 00:00:27 +0400

    * octave compiled with ILL string print (c++ problem)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/octave.mk
+++ mk/math/octave.mk
@@ -1,10 +1,8 @@
 CFG_OCTAVE = --prefix=$(ROOT)/octave \
-	--with-blas="-lblas" --with-lapack="-llapack" 
-# --disable-docs \
-#	--disable-readline --disable-java \
-#	  \
-#	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)" \
-#	gl_cv_func_gettimeofday_clobber=no
+	--with-blas="-lblas" --with-lapack="-llapack" \
+	--disable-docs --disable-readline --disable-java \
+	CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" FFLAGS="$(TOPT)" \
+	gl_cv_func_gettimeofday_clobber=no
 ## ?octave_cv_fortran_integer_size
 ## ?octave_cv_ieee754_data_format
 .PHONY: octave
@@ -13,7 +11,3 @@ octave: $(SRC)/$(OCTAVE)/README
 	cd $(TMP)/$(OCTAVE) &&\
 	$(XPATH) $(SRC)/$(OCTAVE)/$(TCFG) $(CFG_OCTAVE) &&\
 	$(MAKE) && $(INSTALL)
-	
-.PHONY: octave-mk
-octave-mk:	
-	cd $(TMP)/$(OCTAVE) && $(MAKE) && $(INSTALL)
