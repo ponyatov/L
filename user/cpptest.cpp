76fca56c289483c6799802b86a7884fed62fddca
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-25 17:42:33 +0400

    + user/cpptest printf added for test (works)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- user/cpptest.cpp
+++ user/cpptest.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	cerr << endl << argv[0] << endl;
+	printf("\n[%s]\n",argv[0]);
+	cout << endl << "argv[0]" << endl;
+	cout << endl << argv[0] << endl;
+	cout.flush();
 	return 0;
 }
