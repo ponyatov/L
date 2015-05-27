8a213b7aff4f8bc871b429e63999e3e6760ea612
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-27 09:10:53 +0400

    * user/cpptest from linux.org.ru
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- user/cpptest.cpp
+++ user/cpptest.cpp
@@ -1,9 +1,4 @@
 #include <iostream>
-
-using namespace std;
-
-int main(int argc, char *argv[]) {
-	cout << endl << argv[0] << endl;
-//	cout << "\nC++ test passes\n" << endl;
-	return 0;
+int main(int argc, char **argv) {
+    std::cout << argv[0] << std::endl;
 }
