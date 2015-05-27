16c2eeeeca32fde6f131a35f1c82dfc00bf1a95b
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-27 09:10:29 +0400

    + QEMU_CFG : QEMU_(HW|CPU|ARCH|APP)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/math.mk
+++ app/math.mk
@@ -1,3 +1,6 @@
 # app: math
 LIBS = zlib pcre blas lapack 
 APPS = hello cpptest octave
+
+QEMU_VGA = vga=5
+QEMU_APP = -m 256M -smp 4
--- arch/i386.mk
+++ arch/i386.mk
@@ -1,2 +1,4 @@
 # arch: ARCH_X86 i386
 CFG_ARCH = --disable-multilib
+
+QEMU_ARCH = -M pc
