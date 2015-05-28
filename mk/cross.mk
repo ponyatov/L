de9d7a415465c85c393be730190a947432ee2fa5
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-29 01:44:07 +0400

    * cross/ILL
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -3,13 +3,14 @@ CFG_BINUTILS0 = --target=$(TARGET) $(CFG_ARCH) $(CFG_CPU) \
 	--with-sysroot=$(ROOT) \
 	--with-native-system-header-dir=/include \
 	--enable-lto \
-	CFLAGS="$(BOPT)" CXXFLAGS="$(BOPT)"
-#	CFLAGS_FOR_BUILD="-g0 -Ofast -march=native -mtune=native"
+	CFLAGS_FOR_TARGET="$(TOPT)" CXXFLAGS_FOR_TARGET="$(TOPT)"
+#	CFLAGS_FOR_BUILD="$(BOPT)" CXXFLAGS_FOR_BUILD="$(BOPT)"
 
 CFG_WITHCCLIBS = --with-gmp=$(TC) --with-mpfr=$(TC) --with-mpc=$(TC) \
-	--with-isl=$(TC) --with-cloog=$(TC)  
+	--with-isl=$(TC) --with-cloog=$(TC)
 
-CFG_CCLIBS00 = --disable-shared CFLAGS="$(BOPT)"
+CFG_CCLIBS00 = --disable-shared 
+#CFLAGS="$(BOPT)"
 CFG_CCLIBS0  =  $(CFG_WITHCCLIBS) $(CFG_CCLIBS00)
 	
 CFG_GMP0 = $(CFG_CCLIBS0)
@@ -27,9 +28,15 @@ CFG_GCC = $(CFG_BINUTILS0) $(CFG_WITHCCLIBS) --disable-bootstrap \
 	--enable-libstdcxx-time \
 	--enable-libstdcxx-threads \
 	--enable-libstdcxx-pch
+#	--enable-__cxa_atexit
 
 .PHONY: cross0
-cross0: binutils0 cclibs0 ramclean gcc0 ramclean
+cross0:
+	make binutils0
+	make cclibs0
+	make ramclean
+	make gcc0
+	make ramclean
 
 .PHONY: binutils0
 binutils0: $(SRC)/$(BINUTILS)/README
@@ -90,12 +97,19 @@ gccall:
 	cd $(TMP)/$(GCC) && $(MAKE) all-target-libgcc
 	cd $(TMP)/$(GCC) && $(MAKE) install-target-libgcc
 
-,PHONY: gccpp
+.PHONY: gcclibsinst
+gcclibsinst:
+	cp -a $(TC)/$(TARGET)/lib/libstdc++.so* $(LIB)/ && rm $(LIB)/libstdc++*.py
+	cp -a $(TC)/$(TARGET)/lib/libgcc_s.so* $(LIB)/ 
+#	cp $(TC)/$(TARGET)/lib/lib*.a $(LIB)/
+#	cp $(TC)/$(TARGET)/lib/lib*.la $(LIB)/
+
+.PHONY: gccpp
 gccpp:
 	make gccall
 	cd $(TMP)/$(GCC) && $(MAKE) all-target-libstdc++-v3
 	cd $(TMP)/$(GCC) && $(MAKE) install-target-libstdc++-v3
-	cp -a $(TC)/$(TARGET)/lib/libgcc_s* $(LIB)/
+	make gcclibsinst
 
 .PHONY: gcc
 gcc: $(SRC)/$(GCC)/README
