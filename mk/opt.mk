622a1a1d54e721ae2a0ff1b53426d61d01995ec2
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-30 15:01:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/opt.mk
+++ mk/opt.mk
@@ -2,5 +2,6 @@ BOPT = -O2 -g0 -march=native -mtune=native
 
 #OPT_LTO = -flto
 OPT_LTO =
+# use LTO only for user apps: ulibc build with errors if -flto used
 OPT_DEBUG = -g0
 TOPT = -Ofast $(OPT_DEBUG) $(CFLAGS_CPU) -ffast-math $(OPT_LTO)
