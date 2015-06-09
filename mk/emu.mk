9a412baa4c5b2804cfea3fdea2d3b54a0b2411d3
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-09 18:54:08 +0400

    + emuipx netboot
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/emu.mk
+++ mk/emu.mk
@@ -1,6 +1,6 @@
 
 QEMU_NET = -net nic -net tap,ifname=tap0,script=no,downscript=no
-QEMU_ALL =  -m 64M $(QEMU_NET) -localtime -append "$(QEMU_APPEND)"
+QEMU_ALL =  -m 64M $(QEMU_NET) -localtime
 QEMU_SERIAL_LOG = -serial file:ttyS0.log -append "console=ttyS0,115200"
 
 QEMU_CFG = $(QEMU_ALL) $(QEMU_HW) $(QEMU_ARCH) $(QEMU_CPU) $(QEMU_APP)
@@ -8,8 +8,12 @@ QEMU_CFG = $(QEMU_ALL) $(QEMU_HW) $(QEMU_ARCH) $(QEMU_CPU) $(QEMU_APP)
 .PHONY: emu
 emu: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
 	qemu-system-$(ARCH) $(QEMU_CFG) \
-	-kernel $(BOOT)/$(HW)$(APP).kernel \
+	-kernel $(BOOT)/$(HW)$(APP).kernel -append "$(QEMU_APPEND)" \
 	-initrd $(BOOT)/$(HW)$(APP).rootfs
+	
+.PHONY: emuipx
+emuipx: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
+	qemu-system-$(ARCH) $(QEMU_CFG) -boot n
 
 .PHONY: emucluster
 emucluster: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
@@ -21,7 +25,7 @@ emucluster: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
 .PHONY: emuk
 emuk: $(BOOT)/$(HW)$(APP).kernel
 	qemu-system-$(ARCH) $(QEMU_CFG) $(QEMU_ALL) \
-	-kernel $(BOOT)/$(HW)$(APP).kernel \
+	-kernel $(BOOT)/$(HW)$(APP).kernel -append "$(QEMU_APPEND)" \
 	$(QEMU_SERIAL_LOG)
 
 .PHONY: emuiso
