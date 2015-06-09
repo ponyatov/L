c55ddab6f78c784289997b7cd2f330bfcd087f2a
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-09 13:49:44 +0400

    + APPS += cpptest
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/clock.mk
+++ app/clock.mk
@@ -1,6 +1,8 @@
 # app: clock
 LIBS = zlib png sdl
-APPS = $(USRBIN)/hello $(USRBIN)/sdl_hello $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
+APPS = \
+	$(USRBIN)/hello $(USRBIN)/cpptest \
+	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
 
 QEMU_APPEND += vga=0x315
 QEMU_APP = 
