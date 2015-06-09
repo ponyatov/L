9827da39918b685e9c4324412e476cfaada4af2f
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-09 13:43:25 +0400

    * mk/lib/ pcre zib
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/lib/pcre.mk
+++ mk/lib/pcre.mk
@@ -1,4 +1,5 @@
-CFG_PCRE = CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)"
+CFG_PCRE = CFLAGS="$(TOPT)" CXXFLAGS="$(TOPT)" \
+	--enable-utf8 --enable-jit --enable-pcregrep-libz
 .PHONY: pcre
 pcre: $(SRC)/$(PCRE)/README
 	rm -rf $(TMP)/$(PCRE) && mkdir $(TMP)/$(PCRE) &&\
