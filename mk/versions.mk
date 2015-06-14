1cfac85ea7ba0a1471532f74eb202e775f53b5c4
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-14 20:26:37 +0400

    * isl 0.11.1 for binutils build
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/versions.mk
+++ mk/versions.mk
@@ -7,8 +7,8 @@ BINUTILS_VER = 2.24
 GMP_VER = 5.1.3
 MPFR_VER = 3.1.2
 MPC_VER = 1.0.2
-ISL_VER = 0.12.2
-# 0.14 not works with gcc 4.9.x
+ISL_VER = 0.11.1
+# 0.11 need for binutils build
 CLOOG_VER = 0.18.1
 GCC_VER = 4.9.2
 # 4.9.2 used: bug arm/62098 fixed
