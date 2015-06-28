eec50e8044f64ef2b59f2b8631c1de203c665315
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-28 20:24:49 +0400

    +app/dos *mk/head
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/dos.mk
+++ app/dos.mk
@@ -2,7 +2,7 @@
 LIBS = zlib png sdl
 APPS = \
 	$(USRBIN)/hello $(USRBIN)/cpptest \
-	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
+	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect
 
 QEMU_APPEND += vga=0x315
 QEMU_APP = 
