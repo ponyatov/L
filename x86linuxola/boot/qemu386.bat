276d4c14117ef3191a5be7f55832e39e963fecfb
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-19 18:51:45 +0400

    * qemu386 sound fixed: es1370 & HDA only, no sb16

--- x86linuxola/boot/qemu386.bat
+++ x86linuxola/boot/qemu386.bat
@@ -1,3 +1,4 @@
 rem wget -c https://raw.githubusercontent.com/ponyatov/L/linuxola/x86linuxola/boot/x86linuxola.iso
-C:\QEMU\qemu-system-i386w -m 32M -localtime -boot d -cdrom x86linuxola.iso
+rem wget -c http://192.168.0.128/~ponyatov/x86linuxola.iso
+C:\QEMU\qemu-system-i386w -m 32M -boot d -cdrom x86linuxola.iso -soundhw sb16,es1370,hda
 pause
