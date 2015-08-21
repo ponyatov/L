301b1203512360dfacc355b53d1365d566c78a79
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-21 12:24:00 +0400

    * ffmpeg
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/mmedia/codecs.mk
+++ mk/mmedia/codecs.mk
@@ -4,15 +4,14 @@ CFG_FFMPEG = --prefix=$(ROOT)/ffmpeg \
 	--enable-cross-compile --cross-prefix=$(TARGET)- \
 	--disable-everything --disable-network \
 	--enable-yasm --enable-zlib --enable-decoder=gsm \
-	--enable-indev=alsa --enable-outdev=alsa,fbdev,sdl \
+	--enable-indev=alsa,fbdev,v4l2 --enable-outdev=alsa,fbdev,v4l2,sdl \
 	--enable-shared $(CPU_FFMPEG)
-#	--list-outdevs \
+#	--list-indevs \
 
 .PHONY: ffmpeg
 ffmpeg: $(SRC)/$(FFMPEG)/README
 	rm -rf $(TMP)/$(FFMPEG) && mkdir $(TMP)/$(FFMPEG) &&\
 	cd $(TMP)/$(FFMPEG) &&\
-	$(XPATH) $(SRC)/$(FFMPEG)/configure $(CFG_FFMPEG)
-#	 &&\
-#	$(MAKE)
+	$(XPATH) $(SRC)/$(FFMPEG)/configure $(CFG_FFMPEG) &&\
+	$(MAKE)
 #	 && $(INSTALL)-strip
