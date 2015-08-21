0de83c8f2363668e085591f653db43c7146ee050
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-21 14:10:57 +0400

    + win32 syslinux install scripts
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- boot/syslinux/syslinst.bat
+++ boot/syslinux/syslinst.bat
@@ -12,13 +12,12 @@ unzip -jn syslinux-6.03.zip bios\com32\menu\menu.c32
 unzip -jn syslinux-6.03.zip bios\com32\menu\vesamenu.c32
 
 mkdir %USB%\syslinux
+mkdir %USB%\x86linuxola
 syslinux.exe -m -i -d syslinux %USB%
 copy syslinux.cfg %USB%\syslinux\
 copy *.c32 %USB%\syslinux\
 copy memtest.krn %USB%\syslinux\
-copy ..\..\share\splash640x480.png %USB%\syslinux\
-copy dos\io.sys %USB%\dos\
-copy dos\msdos.sys %USB%\dos\
-copy dos\command.com %USB%\dos\
-copy debian\hdinstall.bat %USB%\debian\
+copy ..\..\share\splash640x480.png %USB%\x86linuxola\
+copy ..\..\x86linuxola\boot\x86linuxola.kernel %USB%\x86linuxola\
+copy ..\..\x86linuxola\boot\x86linuxola.rootfs %USB%\x86linuxola\
 pause
