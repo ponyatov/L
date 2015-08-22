1df4079bfc4cea96b241ed57a032533a90c19c13
Dmitry Ponyatov <ponyatov@gac.home>
2015-08-22 08:44:54 +0400

    + alsa_utils (not works)
    Signed-off-by: Dmitry Ponyatov <ponyatov@gac.home>

--- mk/mmedia/alsa.mk
+++ mk/mmedia/alsa.mk
@@ -3,6 +3,7 @@ PHONY: alsa alsa_lib alsa_utils
 alsa: alsa_lib alsa_utils
 
 CFG_ALSA_LIB = --disable-alisp
+CFG_ALSA_UTILS = --disable-alsamixer --disable-xmlto --disable-alsaconf --prefix=$(ROOT)/au
 
 alsa_lib: $(SRC)/$(ALSA_LIB)/README
 	rm -rf $(TMP)/$(ALSA_LIB) && mkdir $(TMP)/$(ALSA_LIB) &&\
@@ -10,5 +11,8 @@ alsa_lib: $(SRC)/$(ALSA_LIB)/README
 	$(XPATH) $(SRC)/$(ALSA_LIB)/$(TCFG) $(CFG_ALSA_LIB) &&\
 	$(MAKE) && $(INSTALL)-strip
 
-alsa_utils:
-	echo $@
+alsa_utils: $(SRC)/$(ALSA_UTILS)/README
+	rm -rf $(TMP)/$(ALSA_UTILS) && mkdir $(TMP)/$(ALSA_UTILS) &&\
+	cd $(TMP)/$(ALSA_UTILS) &&\
+	$(XPATH) $(SRC)/$(ALSA_UTILS)/$(TCFG) $(CFG_ALSA_UTILS) &&\
+	$(MAKE) && $(INSTALL)-strip
