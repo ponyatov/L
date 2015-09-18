99dc9a0dd6bde0eff1f02a79f268bc1d81cba4b5
Dmitry Ponyatov <dponyatov@gmail.com>
2015-09-18 09:56:19 +0400

    * pack: file mask + "-" char
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pack/pack.cpp
+++ pack/pack.cpp
@@ -3,14 +3,10 @@
 int main(){
 	yylex();
 	map<string,int>::iterator it;
-//	cout<<"==============================\n";
-//	cout<<"========== DIRS ==============\n";
-//	cout<<"==============================\n";
+	// ========== DIRS ==============
 	for (it=dirs.begin();it!=dirs.end();it++)
 		cout<<it->first<<"\n";
-//	cout<<"==============================\n";
-//	cout<<"========= FILES ==============\n";
-//	cout<<"==============================\n";
+	// ========== FILES =============
 	for (it=files.begin();it!=files.end();it++)
 		cout<<it->first<<"\n";
 	return 0;
