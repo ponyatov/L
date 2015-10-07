a61334a36168162e4e46413c5528b1a989122ec0
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-07 13:11:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/ulibc.mk
+++ mk/ulibc.mk
@@ -7,9 +7,9 @@ ulibc: $(SRC)/$(ULIBC)/README
 	cd $(SRC)/$(ULIBC) && $(MAKE) $(CFG_ULIBC) allnoconfig
 	# 2
 	cat ulibc/all >> $(SRC)/$(ULIBC)/.config
-	cat ulibc/arch/$(ARCH) >> $(SRC)/$(ULIBC)/.config
-	cat ulibc/cpu/$(CPU) >> $(SRC)/$(ULIBC)/.config
-	cat ulibc/app/$(APP) >> $(SRC)/$(ULIBC)/.config
+	cat arch/$(ARCH).ulibc >> $(SRC)/$(ULIBC)/.config
+	cat cpu/$(CPU).ulibc >> $(SRC)/$(ULIBC)/.config
+	cat app/$(APP).ulibc >> $(SRC)/$(ULIBC)/.config
 	# 3
 	echo "CROSS_COMPILER_PREFIX=\"$(TARGET)-\"" >> $(SRC)/$(ULIBC)/.config
 	echo "KERNEL_HEADERS=\"$(ROOT)/include\"" >> $(SRC)/$(ULIBC)/.config
@@ -18,13 +18,16 @@ ulibc: $(SRC)/$(ULIBC)/README
 	# 5
 	cd $(SRC)/$(ULIBC) && $(MAKE) $(CFG_ULIBC)
 	# 6
-	cd $(SRC)/$(ULIBC) && $(MAKE) $(CFG_ULIBC) install
+	$(call INSTPACK,$(SRC)/$(ULIBC),$@,$(CFG_ULIBC) install)
 	# 7
-	cd $(SRC)/$(ULIBC) && $(MAKE) $(CFG_ULIBC) install_utils
+	$(call INSTPACK,$(SRC)/$(ULIBC),$@-utils,$(CFG_ULIBC) install_utils)
 	# 8
 	cd $(SRC)/$(ULIBC) && $(MAKE) $(CFG_ULIBC) hostutils
 	cp $(SRC)/$(ULIBC)/utils/ldd.host      $(TC)/bin/$(TARGET)-ldd
+	echo $(TC)/bin/$(TARGET)-ldd > $(PACK)/$@-hostutils
 	cp $(SRC)/$(ULIBC)/utils/ldconfig.host $(TC)/bin/$(TARGET)-ldconfig
+	echo $(TC)/bin/$(TARGET)-ldconfig >> $(PACK)/$@-hostutils
 	cp $(SRC)/$(ULIBC)/utils/getconf.host  $(TC)/bin/$(TARGET)-getconf
+	echo $(TC)/bin/$(TARGET)-getconf >> $(PACK)/$@-hostutils
 	# 9 (in root package)
 	# ldconfig in root.mk
