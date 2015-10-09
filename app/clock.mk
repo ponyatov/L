69731b62feede945852208e939ae3fb866ccc71d
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-09 12:02:55 +0400

    * QEMU_APPEND for x86
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/clock.mk
+++ app/clock.mk
@@ -6,5 +6,4 @@ APPS = \
 #	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
 #	$(USRBIN)/cpptest 
 
-QEMU_APPEND += vga=0x312
 QEMU_APP = 
--- hw/x86.mk
+++ hw/x86.mk
@@ -1,5 +1,6 @@
 # universal i386 PC (32 bit) i486sx+
 # (pure i386sx not supported by new gcc/kernel) 
 CPU = i486sx
-QEMU_HW =
 VENDOR = generic
+QEMU_HW =
+QEMU_APPEND += vga=0x312
