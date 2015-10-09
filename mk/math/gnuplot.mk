94f7495091d8dc9527338cd6842be959971fc093
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-09 18:12:18 +0400

    + libgd autoreconf
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/math/gnuplot.mk
+++ mk/math/gnuplot.mk
@@ -1,7 +1,7 @@
-GNUPLOT_CFG = --disable-wxwidgets --disable-history-file
+CFG_GNUPLOT = --disable-wxwidgets --disable-history-file
 
 .PHONY: gnuplot
 gnuplot: $(SRC)/$(GNUPLOT)/README
 	rm -rf $(TMP)/$(GNUPLOT) && mkdir $(TMP)/$(GNUPLOT) && \
 	cd $(TMP)/$(GNUPLOT) &&\
-	$(XPATH) $(SRC)/$(GNUPLOT)/$(TCFG) $(GNUPLOT_CFG)
+	$(XPATH) $(SRC)/$(GNUPLOT)/$(TCFG) $(CFG_GNUPLOT)
