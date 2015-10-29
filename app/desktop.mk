7e26db790665ce2f5ebb702fabe33ee74013239d
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-29 13:49:43 +0400

    + sdl_hello splash app
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/desktop.mk
+++ app/desktop.mk
@@ -1,9 +1,10 @@
 # app: clock
 LIBS = 
 #zlib png sdl
-APPS = $(USRBIN)/hello $(USRBIN)/cpptest 
+APPS = $(USRBIN)/hello $(USRBIN)/cpptest
+#$(USRBIN)/sdl_hello 
 #	$(USRBIN)/sdl_clock \
 #	$(USRBIN)/currency
-#	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
+#	 $(USRBIN)/sdl_rect $(USRBIN)/sdl_clock
 
 QEMU_APP = 
