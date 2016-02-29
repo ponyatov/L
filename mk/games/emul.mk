610fad3fb9a08641f35502d76cfa08dda5b3ce4b
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-29 14:31:33 +0400

    - higan: req X11
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/games/emul.mk
+++ mk/games/emul.mk
@@ -1,4 +1,11 @@
-.PHONY: gz_emul
+.PHONY: gz_emul 
 gz_emul:
-	$(WGET) http://download.byuu.org/higan_v097-source.7z
-	
+
+HIGAN_VER = 097
+HIGAN = higan_v$(HIGAN_VER)-source
+HIGAN_ZIP = $(HIGAN).7z
+.PHONY: gz_higan
+gz_higan: req_X11
+	$(WGET) http://download.byuu.org/$(HIGAN_ZIP)
+	rm -rf $(TMP)/$(HIGAN) ; cd $(TMP) && p7zip -d $(GZ)/$(HIGAN_ZIP)
+	cd $(TMP)/$(HIGAN)/higan && $(MAKE) compiler="$(TCC)"
