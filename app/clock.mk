27eda78b3c5af97ee9e9663ffa24cd724e489f98
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-15 09:45:00 +0400

    * dirs < gambox * app/clock.mk recovered
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ app/clock.mk
@@ -0,0 +1,10 @@
+# app: clock
+LIBS = 
+#zlib png sdl
+APPS = \
+	$(USRBIN)/hello $(USRBIN)/cpptest
+#	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect  
+#	$(USRBIN)/sdl_clock
+#	$(USRBIN)/currency
+
+QEMU_APP = 
