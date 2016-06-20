860b3a9d9842fb657323404da6450ed407f50c97
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-20 18:41:22 +0400

    - drop PACK
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/commands.mk
+++ mk/commands.mk
@@ -31,8 +31,4 @@ RAMCLEAN ?= rm -rf
 
 PACKREP = $(PWD)/pack/pack
 
-INSTPACK = \
-	cd $1 &&\
-	$(XPATH) strace -fs9999 -o $(TMP)/$2.strace make $3 &&\
-	$(PACKREP) < $(TMP)/$2.strace > $(PACK)/$(2) && rm $(TMP)/$2.strace
-#	tar -cmpl -T $(PACK)/$(2) | gzip -c9 - > $(PACK)/$(2).tgz
+INSTPACK = cd $1 && make $3 && touch $(PACK)/$(2)  
