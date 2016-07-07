7b1ae3f532fda51e1e79b28d2c59be0887950600
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-07 12:23:28 +0400

    * root: -pack + dos.cron
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/root.mk
+++ mk/root.mk
@@ -6,6 +6,7 @@ root:
 	rm -rf $(ETC) ; cp -r etc $(ROOT)/
 	cat app/$(APP).rcS >> $(ROOT)/etc/init.d/rcS
 	cp README.md $(ETC)/
+	cp app/$(APP).cron $(ETC)/cron/root 
 	chmod +x $(ETC)/init.d/* $(ETC)/dhcp.rc
 	# 2	
 	$(LDCONFIG) -v -r $(ROOT)
@@ -14,10 +15,6 @@ root:
 	# 4
 	cp -r share $(ROOT)/
 	# 5
-	cd $(ROOT) && find . | egrep -v $(ROOTREX) > $(PACK)/allfiles
-	# 6
-	python ./pack.py $(ROOT)
-#	cat $(PACK)/allfiles > $(PACK)/rootfiles 
-	# 7
-	cd $(ROOT) && cat $(PACK)/rootfiles | cpio -o -H newc > $(BOOT)/$(HW)$(APP).cpio
+	cd $(ROOT) &&\
+	find . | egrep -v $(ROOTREX) | cpio -o -H newc > $(BOOT)/$(HW)$(APP).cpio
 	cat $(BOOT)/$(HW)$(APP).cpio | gzip -9 > $(BOOT)/$(HW)$(APP).rootfs
