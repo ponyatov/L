dbddad8bd450349ea15e283186713c5b8c7105ad
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-07 12:20:03 +0400

    * kernel
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/emu.mk
+++ mk/emu.mk
@@ -1,15 +1,20 @@
 
+QEMU_APPEND += ntpsrv=pool.ntp.org
 QEMU_TAP ?= tap0
 QEMU_NET = -net nic -net tap,ifname=$(QEMU_TAP),script=no,downscript=no
-QEMU_ALL =  -m 64M $(QEMU_NET) -localtime
+QEMU_NET = -net nic -net user
+QEMU_SOUND = -soundhw es1370
+QEMU_ALL =  -m 64M $(QEMU_NET) $(QEMU_SOUND)
+# -localtime
 QEMU_SERIAL_LOG = -serial file:ttyS0.log -append "console=ttyS0,115200"
 
 QEMU_CFG = $(QEMU_ALL) $(QEMU_HW) $(QEMU_ARCH) $(QEMU_CPU) $(QEMU_APP)
 
 .PHONY: emu
 emu: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
-	qemu-system-$(ARCH) $(QEMU_CFG) \
-	-kernel $(BOOT)/$(HW)$(APP).kernel -append "$(QEMU_APPEND)" \
+	qemu-system-$(KERNEL_ARCH) $(QEMU_CFG) \
+	-kernel $(BOOT)/$(HW)$(APP).kernel \
+	-append "$(QEMU_APPEND)" \
 	-initrd $(BOOT)/$(HW)$(APP).rootfs
 	
 .PHONY: pxemu
@@ -26,7 +31,7 @@ emucluster: $(BOOT)/$(HW)$(APP).kernel $(BOOT)/$(HW)$(APP).rootfs
 
 .PHONY: emuk
 emuk: $(BOOT)/$(HW)$(APP).kernel
-	qemu-system-$(ARCH) $(QEMU_CFG) $(QEMU_ALL) \
+	qemu-system-$(ARCH) $(QEMU_CFG) \
 	-kernel $(BOOT)/$(HW)$(APP).kernel -append "$(QEMU_APPEND)" \
 	$(QEMU_SERIAL_LOG)
 
@@ -34,3 +39,7 @@ emuk: $(BOOT)/$(HW)$(APP).kernel
 emuiso: $(BOOT)/$(HW)$(APP).iso
 	qemu-system-$(ARCH) $(QEMU_CFG) \
 	-boot d -cdrom $<
+
+.PHONY: bochs
+bochs: $(BOOT)/$(HW)$(APP).iso
+	bochs -f syslinux/bochs.rc
--- mk/kernel.mk
+++ mk/kernel.mk
@@ -1,28 +1,42 @@
 KERNEL_ARCH ?= $(ARCH)
 CFG_KERNEL = ARCH=$(KERNEL_ARCH) \
-	INSTALL_HDR_PATH=$(ROOT) INSTALL_MOD_PATH=$(ROOT)
-	
-.PHONY: kernel-rpi
-kernel-rpi: $(SRC)/$(KERNEL)/README
-	# 1
-	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) distclean
-	# 2
-	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) bcm2835_defconfig
-	# 3
-	make kernel-all
+	INSTALL_HDR_PATH=$(ROOT) INSTALL_MOD_PATH=$(ROOT) $(HW_KERNEL)
 
 .PHONY: kernel
-kernel: $(SRC)/$(KERNEL)/README
+kernel: kernel-$(VENDOR)
+	touch $(PACK)/$@
+
+.PHONY: kernel-rpi
+$(SRC)/linux/README:
+	# 0
+	cd $(SRC) && git clone --depth 1 git://github.com/raspberrypi/linux.git
+kernel-rpi: $(SRC)/linux/README
+	# 1 bcmrpi_defconfig -> /kernel/hw/rpiB
+	cd $(SRC)/linux && make $(CFG_KERNEL) distclean
+	cd $(SRC)/linux && make $(CFG_KERNEL) $(DEFCONFIG)
+	# 2
+#	cat kernel/all >> $(SRC)/linux/.config
+##	cat arch/$(ARCH).kcfg >> $(SRC)/linux/.config
+##	cat cpu/$(CPU).kcfg >> $(SRC)/linux/.config
+#	cat hw/$(HW).kcfg >> $(SRC)/linux/.config
+#	cat app/$(APP).kcfg >> $(SRC)/linux/.config
+	# 3
+	touch $(PACK)/$@
+	make KERNEL=linux kernel-all
+	
+.PHONY: kernel-generic
+kernel-generic: $(SRC)/$(KERNEL)/README
 	# 1
 	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) distclean
-	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) allnoconfig
+	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) $(DEFCONFIG)
 	# 2
 	cat kernel/all >> $(SRC)/$(KERNEL)/.config
-	cat kernel/arch/$(ARCH) >> $(SRC)/$(KERNEL)/.config
-	cat kernel/cpu/$(CPU) >> $(SRC)/$(KERNEL)/.config
-	cat kernel/hw/$(HW) >> $(SRC)/$(KERNEL)/.config
-	cat kernel/app/$(APP) >> $(SRC)/$(KERNEL)/.config
+	cat arch/$(ARCH).kcfg >> $(SRC)/$(KERNEL)/.config
+	cat cpu/$(CPU).kcfg >> $(SRC)/$(KERNEL)/.config
+	cat hw/$(HW).kcfg >> $(SRC)/$(KERNEL)/.config
+	cat app/$(APP).kcfg >> $(SRC)/$(KERNEL)/.config
 	# 3
+	touch $(PACK)/$@
 	make kernel-all
 	
 .PHONY: kernel-all
@@ -35,12 +49,14 @@ kernel-all:
 	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) menuconfig
 	# 5
 	cd $(SRC)/$(KERNEL) && $(MAKE) $(CFG_KERNEL)
-	-cd $(SRC)/$(KERNEL) && $(MAKE) $(CFG_KERNEL) modules_install
+	-$(call INSTPACK,$(SRC)/$(KERNEL),kernel-modules,$(CFG_KERNEL) modules_install)
 	# 6
 	make kernel-$(ARCH)-fix
-	cp $(SRC)/$(KERNEL)/arch/$(KERNEL_ARCH)/boot/zImage $(BOOT)/$(HW)$(APP).kernel
+	cp	$(SRC)/$(KERNEL)/arch/$(KERNEL_ARCH)/boot/zImage \
+		$(BOOT)/$(HW)$(APP).kernel
+	touch $(PACK)/kernel-image
 	# 7
-	cd $(SRC)/$(KERNEL) && make $(CFG_KERNEL) headers_install
+	$(call INSTPACK,$(SRC)/$(KERNEL),kernel-headers,$(CFG_KERNEL) headers_install)
 	
 .PHONY: kernel-i386-fix
 kernel-i386-fix:
@@ -50,8 +66,12 @@ kernel-i386-fix:
 
 .PHONY: kernel-armel-fix
 kernel-armel-fix:
-	cd $(SRC)/$(KERNEL) && $(MAKE) $(CFG_KERNEL) uImage
-	cp $(SRC)/$(KERNEL)/arch/$(KERNEL_ARCH)/boot/uImage \
-		$(BOOT)/$(HW)$(APP).kernel.uimg
+	make kernel-arm-fix
 .PHONY: kernel-armhf-fix
 kernel-armhf-fix:
+	make kernel-arm-fix
+.PHONY: kernel-arm-fix
+kernel-arm-fix:
+#	cd $(SRC)/$(KERNEL) && $(MAKE) $(CFG_KERNEL) uImage
+#	cp $(SRC)/$(KERNEL)/arch/$(KERNEL_ARCH)/boot/uImage \
+#		$(BOOT)/$(HW)$(APP).kernel.uimg
