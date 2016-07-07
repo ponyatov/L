a1dd994cd57c22014b403e759992276d640566df
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-07 11:01:43 +0400

    * upd cross
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -2,34 +2,25 @@
 CFG_WITHCCLIBS = --with-gmp=$(TC) --with-mpfr=$(TC) --with-mpc=$(TC) \
 	--with-isl=$(TC) --with-cloog=$(TC)
 
-CFG_CCLIBS00 = --disable-shared
-CFG_CCLIBS0  =  $(CFG_WITHCCLIBS) $(CFG_CCLIBS00)
+CFG_CCLIBS00	= --disable-shared
+CFG_CCLIBS0		=  $(CFG_WITHCCLIBS) $(CFG_CCLIBS00)
 
-CFG_GMP0 = $(CFG_CCLIBS0)
-CFG_MPFR0 = $(CFG_CCLIBS0)
-CFG_MPC0 = $(CFG_CCLIBS0)
-CFG_ISL0   = --with-gmp-prefix=$(TC) $(CFG_CCLIBS00)
-CFG_CLOOG0 = --with-gmp-prefix=$(TC) $(CFG_CCLIBS00)
+CFG_GMP0	= $(CFG_CCLIBS0)
+CFG_MPFR0	= $(CFG_CCLIBS0)
+CFG_MPC0	= $(CFG_CCLIBS0)
+CFG_ISL0	= --with-gmp-prefix=$(TC) $(CFG_CCLIBS00)
+CFG_CLOOG0	= --with-gmp-prefix=$(TC) $(CFG_CCLIBS00)
 
 CFG_BINUTILS0 = --target=$(TARGET) $(CFG_ARCH) $(CFG_CPU) \
 	--with-sysroot=$(ROOT) --with-native-system-header-dir=/include \
-	--enable-lto --disable-multilib \
-	CFLAGS_FOR_TARGET="$(TOPT)" CXXFLAGS_FOR_TARGET="$(TOPT)" \
-	$(CFG_WITHCCLIBS)
-#	CFLAGS="$(BOPT)" CXXFLAGS="$(BOPT)"
-#	CFLAGS_FOR_BUILD="$(BOPT)" CXXFLAGS_FOR_BUILD="$(BOPT)"
+	--enable-lto --disable-multilib $(CFG_WITHCCLIBS)
 
 CFG_GCC0 = $(CFG_BINUTILS0) --disable-bootstrap \
 	--disable-shared --disable-threads \
 	--without-headers --with-newlib
-#	CFLAGS_FOR_BUILD="$(BOPT)" CXXFLAGS_FOR_BUILD="$(BOPT)"
 
 CFG_GCC = $(CFG_BINUTILS0) $(CFG_WITHCCLIBS) --disable-bootstrap \
 	--enable-shared --enable-threads --enable-libgomp
-#	--enable-libstdcxx-time \
-#	--enable-libstdcxx-threads \
-#	--enable-libstdcxx-pch
-#	--enable-__cxa_atexit
 
 .PHONY: cross0
 cross0:
@@ -44,84 +35,77 @@ cclibs0: gmp0 mpfr0 mpc0 cloog0 isl0
 
 .PHONY: gmp0
 gmp0: $(SRC)/$(GMP)/README
-	rm -rf $(TMP)/$(GMP) && mkdir $(TMP)/$(GMP) &&\
-	cd $(TMP)/$(GMP) &&\
-	$(SRC)/$(GMP)/$(BCFG) $(CFG_GMP0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	rm -rf $(TMP)/$(GMP) && mkdir $(TMP)/$(GMP) && cd $(TMP)/$(GMP) &&\
+	$(SRC)/$(GMP)/$(BCFG) $(CFG_GMP0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(GMP),$@,install-strip)
 
 .PHONY: mpfr0
 mpfr0: $(SRC)/$(MPFR)/README
-	rm -rf $(TMP)/$(MPFR) && mkdir $(TMP)/$(MPFR) &&\
-	cd $(TMP)/$(MPFR) &&\
-	$(SRC)/$(MPFR)/$(BCFG) $(CFG_MPFR0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	rm -rf $(TMP)/$(MPFR) && mkdir $(TMP)/$(MPFR) && cd $(TMP)/$(MPFR) &&\
+	$(SRC)/$(MPFR)/$(BCFG) $(CFG_MPFR0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(MPFR),$@,install-strip)
 
 .PHONY: mpc0
 mpc0: $(SRC)/$(MPC)/README
-	rm -rf $(TMP)/$(MPC) && mkdir $(TMP)/$(MPC) &&\
-	cd $(TMP)/$(MPC) &&\
-	$(SRC)/$(MPC)/$(BCFG) $(CFG_MPC0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	rm -rf $(TMP)/$(MPC) && mkdir $(TMP)/$(MPC) && cd $(TMP)/$(MPC) &&\
+	$(SRC)/$(MPC)/$(BCFG) $(CFG_MPC0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(MPC),$@,install-strip)
 	
 .PHONY: cloog0
 cloog0: $(SRC)/$(CLOOG)/README
-	rm -rf $(TMP)/$(CLOOG) && mkdir $(TMP)/$(CLOOG) &&\
-	cd $(TMP)/$(CLOOG) &&\
-	$(SRC)/$(CLOOG)/$(BCFG) $(CFG_CLOOG0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	rm -rf $(TMP)/$(CLOOG) && mkdir $(TMP)/$(CLOOG) && cd $(TMP)/$(CLOOG) &&\
+	$(SRC)/$(CLOOG)/$(BCFG) $(CFG_CLOOG0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(CLOOG),$@,install-strip)
 	
 .PHONY: isl0
 isl0: $(SRC)/$(ISL)/README
-	rm -rf $(TMP)/$(ISL) && mkdir $(TMP)/$(ISL) &&\
-	cd $(TMP)/$(ISL) &&\
-	$(SRC)/$(ISL)/$(BCFG) $(CFG_ISL0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	rm -rf $(TMP)/$(ISL) && mkdir $(TMP)/$(ISL) && cd $(TMP)/$(ISL) &&\
+	$(SRC)/$(ISL)/$(BCFG) $(CFG_ISL0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(ISL),$@,install-strip)
 	
 .PHONY: binutils0
 binutils0: $(SRC)/$(BINUTILS)/README
 	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
 	cd $(TMP)/$(BINUTILS) &&\
-	$(SRC)/$(BINUTILS)/$(BCFG) $(CFG_BINUTILS0) &&\
-	$(MAKE) && $(INSTALL)-strip
+	$(SRC)/$(BINUTILS)/$(BCFG) $(CFG_BINUTILS0) && $(MAKE) &&\
+	$(call INSTPACK,$(TMP)/$(BINUTILS),$@,install-strip)
 
-.PHONY: gcc0
-gcc0: $(SRC)/$(GCC)/README
+.PHONY: gcc0 gcc
+gcc0: gcc0-cfg gcc-bin gcc-lib
+	touch $(PACK)/$@
+gcc: gcc-cfg gcc-bin gcc-lib gcc-cpp gcc-inst
+	touch $(PACK)/$@
+gccf: gccf-cfg gcc-bin gcc-lib gcc-cpp gcc-fortran gcc-inst
+	touch $(PACK)/$@
+
+gcc0-cfg: $(SRC)/$(GCC)/README
 	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
 	cd $(TMP)/$(GCC) &&\
 	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC0) --enable-languages="c"
-	make gccall
+	touch $(PACK)/$@
+gcc-cfg: $(SRC)/$(GCC)/README
+	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
+	cd $(TMP)/$(GCC) &&\
+	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC0) --enable-languages="c,c++"
+	touch $(PACK)/$@
+gccf-cfg: $(SRC)/$(GCC)/README
+	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
+	cd $(TMP)/$(GCC) &&\
+	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC0) --enable-languages="c,c++,fortran"
+	touch $(PACK)/$@
 
-.PHONY: gccall
-gccall:
-	cd $(TMP)/$(GCC) && $(MAKE) all-gcc
-	cd $(TMP)/$(GCC) && $(MAKE) install-gcc
-	cd $(TMP)/$(GCC) && $(MAKE) all-target-libgcc
-	cd $(TMP)/$(GCC) && $(MAKE) install-target-libgcc
-
-.PHONY: gcclibsinst
-gcclibsinst:
-	cp -a $(TC)/$(TARGET)/lib/lib* $(LIB)/ && rm $(LIB)/libstdc++*.py
-
-.PHONY: gccpp
-gccpp:
-	make gccall
+gcc-bin:
+	cd $(TMP)/$(GCC) && rm -f $(PACK)/$@ && $(MAKE) all-gcc
+	$(call INSTPACK,$(TMP)/$(GCC),$@,install-gcc)
+gcc-lib:
+	cd $(TMP)/$(GCC) && rm -f $(PACK)/$@ && $(MAKE) all-target-libgcc
+	$(call INSTPACK,$(TMP)/$(GCC),$@,install-target-libgcc)
+gcc-cpp:	
 	cd $(TMP)/$(GCC) && $(MAKE) all-target-libstdc++-v3
-	cd $(TMP)/$(GCC) && $(MAKE) install-target-libstdc++-v3
-
-.PHONY: gcc
-gcc: $(SRC)/$(GCC)/README
-	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
-	cd $(TMP)/$(GCC) &&\
-	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC) --enable-languages="c,c++"
-	make gccpp
-	make gcclibsinst
-
-.PHONY: gccf
-gccf: $(SRC)/$(GCC)/README
-	rm -rf $(TMP)/$(GCC) && mkdir $(TMP)/$(GCC) &&\
-	cd $(TMP)/$(GCC) &&\
-	$(SRC)/$(GCC)/$(BCFG) $(CFG_GCC) --enable-languages="c,c++,fortran"
-	make gccpp
+	$(call INSTPACK,$(TMP)/$(GCC),$@,install-target-libstdc++-v3)
+gcc-fortran:
 	cd $(TMP)/$(GCC) && $(MAKE) all-target-libgfortran
-	cd $(TMP)/$(GCC) && $(MAKE) install-target-libgfortran
-	make gcclibsinst
+	$(call INSTPACK,$(TMP)/$(GCC),$@,install-target-libgfortran)
+gcc-inst:
+	cp -a $(TC)/$(TARGET)/lib/lib* $(LIB)/ && rm $(LIB)/libstdc++*.py
+	touch $(PACK)/$@
