ad22d8824adec85a82cace2c50d7d6a88c387613
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-07 12:33:05 +0400

    * dos .kcfg
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/dos.mk
+++ app/dos.mk
@@ -1,8 +1,9 @@
 # app: clock
-LIBS = zlib png sdl
-APPS = \
-	$(USRBIN)/hello $(USRBIN)/cpptest \
-	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect
+LIBS = 
+#zlib png sdl
+APPS = 
+#	$(USRBIN)/hello $(USRBIN)/cpptest \
+#	$(USRBIN)/sdl_hello $(USRBIN)/sdl_rect
 
 QEMU_APPEND += vga=0x315
 QEMU_APP = 
