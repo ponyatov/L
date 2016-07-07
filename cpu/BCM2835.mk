52a1672f738cf5928e7820333461c16a078f035a
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-07 10:40:49 +0400

    * cpu
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpu/BCM2835.mk
+++ cpu/BCM2835.mk
@@ -1,6 +1,6 @@
 # Raspberry Pi SoC BCM2835: ARM1176JZF-S
 # target arch is ARM11 hard-float
-ARCH = armel
+ARCH = armhf
 # target triplet in armhf.mk
 # target CPU configure params
 CFG_CPU = --with-cpu=arm1176jzf-s
@@ -9,5 +9,6 @@ CFLAGS_CPU = -mcpu=arm1176jzf-s
 #
 ## -march=armv6  -mfpu=vfp -mfloat-abi=hard
 ## http://www.gurucoding.com/en/rpi_cross_compiler/building_binutils_gcc1_cygwin_vm.php
+CFG_CPU_LIBS = --disable-mmx
 
 QEMU_CPU = -cpu arm1176
